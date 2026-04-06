60e3039777c70a9ac8915719253fd25ccd44631f
mridulvaishnav <mriduliiti@gmail.com>
2026-04-06 22:50:59 +0530

    Created the Profile Photo Selector Component

--- frontend/expense-tracker/src/pages/Auth/SignUp.jsx
+++ frontend/expense-tracker/src/pages/Auth/SignUp.jsx
@@ -16,7 +16,30 @@ const SignUp = () => {
   const navigate = useNavigate();
 
   //Handle SignUp Form Submit
-  const handleSignUp = async (e) => {}
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    let profileImageUrl = "";
+
+    if(!fullName) {
+      setError("Please enter your name");
+      return;
+    }
+
+    if(!validateEmail(email)) {
+      setError("Please enter a valid emai address.");
+      return;
+    }
+
+    if(!password) {
+      setError("Please enter the password");
+      return;
+    }
+
+    setError("");
+
+    //SignUp API call
+  }
 
   return (
     <AuthLayout>
@@ -58,6 +81,20 @@ const SignUp = () => {
           </div>
 
           </div>
+
+          {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+
+          <button type="submit" className="btn-primary">
+            SIGNUP
+          </button>
+
+          <p className="text-[13px] text-slate-800 mt-3">
+            Already have an account?{" "}
+            <Link className="font-medium text-primary underline" to="/login">
+              LogIn
+            </Link>
+          </p>
+
         </form>
       </div>
     </AuthLayout>
