0907115a7fd9bda34e2339755990bead7dc27b2b
mridulvaishnav <mriduliiti@gmail.com>
2026-04-18 07:49:07 +0530

    20. Fetching Dashboard Data via API Call

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -1,11 +1,43 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import DashboardLayout from '../../components/layouts/DashboardLayout'
 import { useUserAuth } from '../../hooks/useUserAuth';
 import { useNavigate } from 'react-router-dom';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
 
 
 const Home = () => {
   useUserAuth();
+
+  const navigate = useNavigate();
+
+  const [dashbaordData, setDashboardData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchDashboardData = async () => {
+    if(loading) return;
+
+    setLoading(true);
+
+    try {
+      const response = await axiosInstance.get(
+        `${API_PATHS.DASHBOARD.GET_DATA}`
+      );
+
+      if(response.data) {
+        setDashboardData(response.data);
+      }
+    } catch (error) {
+      console.log("Something went wrong. Please try again.", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchDashboardData();
+    return () => {};
+  }, []);
 
   return (
     <DashboardLayout activeMenu="Dashboard">
