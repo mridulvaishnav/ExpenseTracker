b1872ad6dd72bb40fe5068da64864d424cae112f
mridulvaishnav <mriduliiti@gmail.com>
2026-04-18 07:31:41 +0530

    Created Dashbaord Page Layout

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DashboardLayout from '../../components/layouts/DashboardLayout'
+import { useUserAuth } from '../../hooks/useUserAuth';
+import { useNavigate } from 'react-router-dom';
 
 
 const Home = () => {
   useUserAuth();
-  
+
   return (
     <DashboardLayout activeMenu="Dashboard">
-      <div className = "my-5 mx-auto"></div>
+      <div className = "my-5 mx-auto">
+        Home
+      </div>
     </DashboardLayout>
   )
 }
